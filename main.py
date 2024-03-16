15b176cbf4853dadf54b473b0b36d1c30d9c0f13
Latip176 <latipharkat176@gmail.com>
2024-03-16 23:45:40 +0700

    change structur file

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
-from enum import Enum
-from server import Java, Bedrock
-
-application = FastAPI()
-
-SERVER_TYPES = ["java", "bedrock"]
+import uvicorn
+from os import getenv
 
 
-class ServerType(str, Enum):
-    def __str__(self):
-        return self.value
-
-    JAVA = "java"
-    BEDROCK = "bedrock"
-
-
-@application.exception_handler(Exception)
-async def internal_server_error_handler(request, exc):
-    return JSONResponse(
-        status_code=500,
-        content={"detail": "Internal Server Error"},
-    )
-
-
-@application.get("/api/{ServerTipe}")
-async def handler_request(
-    ServerTipe: ServerType, ip: str, port: int | None = 19132
-) -> dict:
-    if ServerTipe not in SERVER_TYPES:
-        raise HTTPException(status_code=422, detail="type server not found")
-        # return {"msg": "type server is not found"}
-    try:
-        data = Java(ip) if tipe.lower() == "java" else Bedrock(ip, port)
-    except Exception as e:
-        raise HTTPException(status_code=403, detail=e)
-
-    return {"msg": "success", "data": data}
+if __name__ == "__main__":
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("app.api:application", host="0.0.0.0", port=port, reload=True)
