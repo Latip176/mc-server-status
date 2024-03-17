795b1df9b8dda3bae5cef1de8eb919c38e19538c
Latif Harkat <109482864+Latip176@users.noreply.github.com>
2024-03-17 10:24:17 +0700

    Change Fix annotation return Java

--- app/server/__init__.py
+++ app/server/__init__.py
@@ -1,7 +1,7 @@
 from mcstatus import JavaServer, BedrockServer
 
 
-def Java(ip: str) -> BedrockServer:
+def Java(ip: str) -> JavaServer:
     server = JavaServer.lookup(f"{ip}").status()
     return server
 
